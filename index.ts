9ac5f1d3c6d46592161be5a4a0597c48000d403b
sjennolkaya <sjennol@hotmail.com>
2020-09-07 23:07:50 +0200

    1 file modified

--- index.ts
+++ index.ts
@@ -10,16 +10,16 @@ let output = document.getElementById('p');
 //
 // will calculate next iteration step on fibonacci sequence
 //
-let m = map(([next, prev, l]) => [next + prev, next, l - 1]);
+let m = map(([next, prev, index]) => [next + prev, next, index - 1]);
 wrap(fromEvent(textbox, 'input'))                    // --> wrap the `Observable` in a `Pin`
   .to(pipe(debounceTime(1000)))                // --> wait for people to type in the number
   .to(map(() => parseInt((textbox as any).value)))   // --> map the input event to value of the input
   .to(map(n => [1, 0, n]))                     // --> map the number to start iteration
-  .to(filter(([_, __, l]) => l >= 0))          // --> check if we should do any iteration
+  .to(filter(([_, __, i]) => i >= 0))          // --> check if we should do any iteration
   .to(sink(t => console.clear()))              // --> clear console  
   .to(m)                                       // --> calculate next step
   .to(pipe(delay(300)))                        // --> take a breath
-  .to(filter(([_, __, l]) => l > 0))           // --> check if we should continue
+  .to(filter(([_, __, i]) => i > 0))           // --> check if we should continue
   .to(m)                                       // --> back to the loop
   .to(map(([_, f, __]) => f))                  // --> btw, lets take each number in the sequence
   .to(sink(() => output.classList.add('faded')))    // --> fade the <p> who is going to show the value
